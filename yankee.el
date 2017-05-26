17261fc8c0f5837d701206f71e9478ac08877d68
Jake Romer <jkrmr.io@gmail.com>
2017-05-25 23:47:15 -0400

    Replace missing quote in anchor tag template

--- yankee.el
+++ yankee.el
@@ -184,7 +184,7 @@ Currently only supports Git."
   (insert "```" language "\n")
   (goto-char (point-max))
   (insert "\n\n" code "```\n")
-  (and url (insert (format "<sup>\n  <a href=%s\">\n    %s\n  </a>\n</sup>\n" url path)))
+  (and url (insert (format "<sup>\n  <a href=\"%s\">\n    %s\n  </a>\n</sup>\n" url path)))
   (insert "</details>"))
 
 (defun yankee--org-code-fence (language code path url)
